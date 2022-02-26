26d1756f284f5d3ddbe2c52d1b2cacbab775b459
Luke Perry <nperry@terpmail.umd.edu>
2022-02-25 22:01:31 -0500

    trying interpreter, giving up

--- a3/interp.rkt
+++ a3/interp.rkt
@@ -18,7 +18,19 @@
          (interp e2)
          (interp e3))]
     ;; TODO: Handle cond
+    [(Cond e1 e2)
+     (interp-clauses e1)]
+       ;;(cond
+         ;;(define something
+          ;; (match e1
+           ;; [(Clause e1 e2) (list (interp e1) (interp e2))]))
+         ;;[else e2])]
+       
+    )
     ;; TODO: Handle case
-    ))
+)
 
-
+(define (interp-clauses s)
+  (for/list ([i s])
+    (match i
+      [(Clause e1 e2) (list (interp e1) (interp e2))])))
