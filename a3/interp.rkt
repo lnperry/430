4d8afbf7c04be2c5c56cd0bb0e75053373153b7d
Luke Perry <nperry@terpmail.umd.edu>
2022-02-27 21:53:01 -0500

    interp for now on basic case stmts, must check nested which I think will fail

--- a3/interp.rkt
+++ a3/interp.rkt
@@ -18,21 +18,49 @@
          (interp e2)
          (interp e3))]
     ;; TODO: Handle cond
-    [(Cond e1 e2)
-     (let ((c1 (interp-clauses e1))) 
-       (if c1
-         c1
-         (interp e2)))]
+    [(Cond cs e)
+     (let ((cls (interp-cond-clauses cs))) 
+       (if cls
+         cls
+         (interp e)))]
+    [(Case e cs el)
+     (let ((cls (interp-case-clauses (interp e) cs))) 
+       (if (equal? #f cls)
+         (interp el)
+         cls))]
+       ;;(if cls
+        ;; (interp el)
+         ;;(first cls)))]
     )
     ;; TODO: Handle case
 )
 
-(define (interp-clauses e)
+(define (interp-cond-clauses e)
   (match e
     [(cons x xs) (match x
-                   [(Clause e1 e2) (if (interp e1)
-                          (interp e2)
-                          (interp-clauses xs))]
+                   [(Clause e1 e2) 
+                    (if (interp e1)
+                      (interp e2)
+                      (interp-cond-clauses xs))]
                    ['() #f])]
     ['() #f]))
 
+
+(define (interp-case-clauses m e)
+  (match e
+    [(cons x xs) (match x
+                   [(Clause e1 e2) 
+                    (let ((matchval (find-clause-match m e1 e2)))
+                     (if (equal? #f matchval)
+                       matchval ;; no matches in this leftClause list
+                       (interp e2)))]
+                   ['() '()])]
+    ['() #f]))
+
+(define (find-clause-match m e1 e2)
+  (match e1
+    [(cons x xs) (if (equal? m (interp x))
+                  e2 ;; if returned, interp in fx above
+                   (find-clause-match m xs e2))]
+                   ;;(list "m" m "e1" e1 "x" x "xs" xs "equal?" (equal? m (interp x)))]
+    ['() #f]))
