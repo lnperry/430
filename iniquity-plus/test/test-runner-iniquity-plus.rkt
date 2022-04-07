6a0e6845018ef7c46d83c88f2e63328dd0fc96d5
Luke Perry <nperry@terpmail.umd.edu>
2022-04-07 19:27:16 -0400

    add to racket notes, update tets to run

--- iniquity-plus/test/test-runner-iniquity-plus.rkt
+++ iniquity-plus/test/test-runner-iniquity-plus.rkt
@@ -8,37 +8,37 @@
   (check-equal? (run
                  '(define (f x) x)
                  '(f 5))
-                5))
-  ; (check-equal? (run
-                 ; '(define (tri x)
-                    ; (if (zero? x)
-                        ; 0
-                        ; (+ x (tri (sub1 x)))))
-                 ; '(tri 9))
-                ; 45)
-;
-  ; (check-equal? (run
-                 ; '(define (even? x)
-                    ; (if (zero? x)
-                        ; #t
-                        ; (odd? (sub1 x))))
-                 ; '(define (odd? x)
-                    ; (if (zero? x)
-                        ; #f
-                        ; (even? (sub1 x))))
-                 ; '(even? 101))
-                ; #f)
-;
-  ; (check-equal? (run
-                 ; '(define (map-add1 xs)
-                    ; (if (empty? xs)
-                        ; '()
-                        ; (cons (add1 (car xs))
-                              ; (map-add1 (cdr xs)))))
-                 ; '(map-add1 (cons 1 (cons 2 (cons 3 '())))))
-                ; '(2 3 4))
-;
-  ; Iniquity+
+                5)
+  (check-equal? (run
+                 '(define (tri x)
+                    (if (zero? x)
+                        0
+                        (+ x (tri (sub1 x)))))
+                 '(tri 9))
+                45)
+
+  (check-equal? (run
+                 '(define (even? x)
+                    (if (zero? x)
+                        #t
+                        (odd? (sub1 x))))
+                 '(define (odd? x)
+                    (if (zero? x)
+                        #f
+                        (even? (sub1 x))))
+                 '(even? 101))
+                #f)
+
+  (check-equal? (run
+                 '(define (map-add1 xs)
+                    (if (empty? xs)
+                        '()
+                        (cons (add1 (car xs))
+                              (map-add1 (cdr xs)))))
+                 '(map-add1 (cons 1 (cons 2 (cons 3 '())))))
+                '(2 3 4))
+  
+  ;; Iniquity+
   ; (check-equal? (run '(define (f x) x)
                      ; '(f))
                 ; 'err)
@@ -143,11 +143,11 @@
                         ; (apply append xs))
                      ; '(flatten (list (append) (append (list 1 2 3) (list 4 5) (list 6)) (list 7))))
                 ; '(1 2 3 4 5 6 7))
-  ; )
-;
-(define (test-runner-io run)
+  )
 
-   Iniquity examples
+(define (test-runner-io run)
+ 
+  ;; Iniquity examples
   (check-equal? (run ""
                      '(define (print-alphabet i)
                         (if (zero? i)
@@ -157,37 +157,37 @@
                      '(print-alphabet 26))
                 (cons (void) "abcdefghijklmnopqrstuvwxyz"))
 
-  ; (check-equal? (run ""
-  ;                    '(define (f x)
-  ;                       (write-byte x))
-  ;                    '(f 97))
-  ;               (cons (void) "a"))
-  ; (check-equal? (run ""
-  ;                    '(define (f x y)
-  ;                       (write-byte x))
-  ;                    '(f 97 98))
-  ;               (cons (void) "a"))
-  ; (check-equal? (run ""
-  ;                    '(define (f x)
-  ;                       (let ((y x))
-  ;                         (write-byte y)))
-  ;                    '(f 97))
-  ;               (cons (void) "a"))
-  ; (check-equal? (run ""
-  ;                    '(define (f x y)
-  ;                       (let ((y x))
-  ;                         (write-byte y)))
-  ;                    '(f 97 98))
-  ;               (cons (void) "a"))
-  ; (check-equal? (run ""
-  ;                    '(define (f x)
-  ;                       (write-byte x))
-  ;                    '(let ((z 97))
-  ;                       (f z)))
-  ;               (cons (void) "a"))
-  ; (check-equal? (run ""
-  ;                    '(define (f x y)
-  ;                       (write-byte x))
-  ;                    '(let ((z 97))
-  ;                       (f z 98)))
-                ;cons (void) "a")))
+  (check-equal? (run ""
+                     '(define (f x)
+                        (write-byte x))
+                     '(f 97))
+                (cons (void) "a"))
+  (check-equal? (run ""
+                     '(define (f x y)
+                        (write-byte x))
+                     '(f 97 98))
+                (cons (void) "a"))
+  (check-equal? (run ""
+                     '(define (f x)
+                        (let ((y x))
+                          (write-byte y)))
+                     '(f 97))
+                (cons (void) "a"))
+  (check-equal? (run ""
+                     '(define (f x y)
+                        (let ((y x))
+                          (write-byte y)))
+                     '(f 97 98))
+                (cons (void) "a"))
+  (check-equal? (run ""
+                     '(define (f x)
+                        (write-byte x))
+                     '(let ((z 97))
+                        (f z)))
+                (cons (void) "a"))
+  (check-equal? (run ""
+                     '(define (f x y)
+                        (write-byte x))
+                     '(let ((z 97))
+                        (f z 98)))
+                (cons (void) "a")))
