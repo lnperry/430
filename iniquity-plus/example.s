4c8ddfb444b0b534f4e1b0deb79b31911d416966
Luke Perry <nperry@terpmail.umd.edu>
2022-04-13 20:13:37 -0400

    debug for pc

--- iniquity-plus/example.s
+++ iniquity-plus/example.s
@@ -8,15 +8,17 @@
         global entry
 entry:
         mov rbx, rdi
+;;; Start compile-let
         mov rax, 152
         push rax
+;;; Start compile-appply
         lea rax, [rel ret667]
         push rax
         mov r8, 1               ; Moving length of es into r8
         mov rax, 32
         push rax
         ;; Compile e starting
-        mov rax, [rsp + 0]
+        mov rax, [rsp + 16]
 ;;; Compile e done
                                 ; Start of compile-e-list
         cmp rax, 152
@@ -45,27 +47,8 @@ ret667:
         add rsp, 8
         ret
 label_append_bd8f7e2d3b0458:
-        cmp r8, 0
-        jl raise_error_align
-        sub r8, 0
-        mov r9, r8
-        mov rax, 152
-        cmp r9, 0
-        je donePop674
-        jmp condPop673
-loopPop672:
-        mov [rbx + 0], rax
-        pop rax
-        mov [rbx + 8], rax
-        mov rax, rbx
-        or rax, 2
-        add rbx, 16
-        sub r9, 1
-condPop673:
-        cmp r9, 0
-        jne loopPop672
-donePop674:
-        push rax
+        cmp r8, 1
+        jne raise_error_align
         mov rax, [rsp + 0]
         add rsp, 8
         ret
